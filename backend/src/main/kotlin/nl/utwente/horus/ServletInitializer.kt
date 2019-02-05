c325050a4e7d0c36dbe2e05e74f24e8b59299d36
Harindu Perera <harinduenator@gmail.com>
2019-02-05 18:30:13 +0100

    Fix wrong initializer change

--- backend/src/main/kotlin/nl/utwente/horus/ServletInitializer.kt
+++ backend/src/main/kotlin/nl/utwente/horus/ServletInitializer.kt
@@ -2,17 +2,11 @@ package nl.utwente.horus
 
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import javax.servlet.ServletContext
 
 class ServletInitializer : SpringBootServletInitializer() {
 
 	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
 		return application.sources(HorusApplication::class.java)
-	}
-
-	override fun onStartup(servletContext: ServletContext) {
-		servletContext.setInitParameter("spring.profiles.active", "develop")
-		super.onStartup(servletContext)
 	}
 
 }
