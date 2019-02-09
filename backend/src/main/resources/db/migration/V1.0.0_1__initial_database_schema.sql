14f39ecda0f6087312c7f4cfdda4b26fd39b39d9
Rick de Vries <11596702+VriesDeRick@users.noreply.github.com>
2019-02-09 16:06:27 +0100

    Update V1.0.0_1__initial_database_schema.sql

--- backend/src/main/resources/db/migration/V1.0.0_1__initial_database_schema.sql
+++ backend/src/main/resources/db/migration/V1.0.0_1__initial_database_schema.sql
@@ -138,7 +138,7 @@ CREATE TABLE assignment (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   assignment_set_id BIGINT NOT NULL REFERENCES assignment_set(id),
   name VARCHAR NOT NULL,
-  comment_thread_id BIGINT NOT NULL,
+  comment_thread_id BIGINT NULL REFERENCES comment_thread(id) ON DELETE SET NULL ,
   orderKey VARCHAR NOT NULL,
   created_by BIGINT NOT NULL REFERENCES participant(id),
   created_at TIMESTAMP WITH TIME ZONE NOT NULL
