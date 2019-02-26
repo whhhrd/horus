d4f7d808f4c4e3479943e96fc357e01109bd4d1c
Rick de Vries <11596702+VriesDeRick@users.noreply.github.com>
2019-02-26 08:45:54 +0100

    Use InputStream for populating DB from scripts (#89)
    * Changed ResourceUtil to InputStream with BufferedReader
    
    * Change resource loader

--- backend/src/main/kotlin/nl/utwente/horus/DatabasePopulator.kt
+++ backend/src/main/kotlin/nl/utwente/horus/DatabasePopulator.kt
@@ -1,18 +1,12 @@
 package nl.utwente.horus
 
 import nl.utwente.horus.entities.course.CourseRepository
-import org.hibernate.Session
-import org.hibernate.SessionFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
-import org.springframework.core.io.ClassPathResource
-import org.springframework.core.io.support.EncodedResource
-import org.springframework.jdbc.datasource.init.ScriptUtils
 import org.springframework.stereotype.Component
-import org.springframework.util.ResourceUtils
-import java.io.File
-import java.sql.Connection
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import javax.persistence.EntityManagerFactory
 
 /**
@@ -48,7 +42,8 @@ class DatabasePopulator {
             )
             scriptNames.forEach {script: String ->
                 try {
-                    val sql = ResourceUtils.getFile("classpath:db/mockdata/$script").readText()
+                    val stream = this::class.java.getResourceAsStream("/db/mockdata/$script")
+                    val sql = BufferedReader(InputStreamReader(stream))
                     sql.lines().forEach { line: String ->
                         val query = manager.createNativeQuery(line)
                         query.executeUpdate()
