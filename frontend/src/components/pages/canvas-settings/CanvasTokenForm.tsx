44bdfc4fc389bcd3f03bf281b2fdeddc692c350c
Justin Praas <j.w.praas@student.utwente.nl>
2019-04-02 09:48:29 +0200

    Add tutorial (#303)

--- frontend/src/components/pages/canvas-settings/CanvasTokenForm.tsx
+++ frontend/src/components/pages/canvas-settings/CanvasTokenForm.tsx
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import { Formik, Field } from "formik";
 import { connect } from "react-redux";
-import { tokenSubmitAction, checkTokenAndRedirectImportAction } from "../../../state/canvas-settings/actions";
+import {
+    tokenSubmitAction,
+    checkTokenAndRedirectImportAction,
+} from "../../../state/canvas-settings/actions";
 import { buildContent } from "../../pagebuilder";
-import { Form, Spinner, FormGroup, Label, Input, Button, Row, Col } from "reactstrap";
+import {
+    Form,
+    Spinner,
+    FormGroup,
+    Label,
+    Input,
+    Button,
+    Row,
+    Col,
+} from "reactstrap";
 
 interface CanvasTokenFormProps {
-    submitToken: (token: string) => {
+    submitToken: (
+        token: string,
+    ) => {
         type: string;
     };
 
@@ -23,8 +37,10 @@ interface CanvasTokenValues {
     token: string;
 }
 
-class CanvasTokenForm extends Component<CanvasTokenFormProps, CanvasTokenFormState> {
-
+class CanvasTokenForm extends Component<
+    CanvasTokenFormProps,
+    CanvasTokenFormState
+> {
     constructor(props: CanvasTokenFormProps) {
         super(props);
         this.state = {
@@ -52,31 +68,49 @@ class CanvasTokenForm extends Component<CanvasTokenFormProps, CanvasTokenFormSta
             return (
                 <Row>
                     <Col xs="12" md="6" className="mx-auto">
+                        <h3>Token import</h3>
+                        In order to import a token, you should visit{" "}
+                        <a
+                            href="https://canvas.utwente.nl/profile/settings"
+                            target="_blank"
+                        >
+                            the canvas settings page
+                        </a>
+                        , generate a token and copy the generated token in the
+                        box below.
                         <Formik
                             initialValues={{ token: "" }}
-                            onSubmit={this.onSubmit}>
+                            onSubmit={this.onSubmit}
+                        >
                             {({ handleSubmit }) => (
-                                <Form>
+                                <Form className="mt-2">
                                     <FormGroup>
                                         <Label>Token</Label>
-                                        <Input tag={Field} id="token" name="token" />
+                                        <Input
+                                            tag={Field}
+                                            id="token"
+                                            name="token"
+                                        />
                                     </FormGroup>
-                                    <Button color="primary"
-                                        onClick={() => handleSubmit()}>
+                                    <Button
+                                        color="primary"
+                                        onClick={() => handleSubmit()}
+                                    >
                                         Submit
-                            </Button>
+                                    </Button>
                                 </Form>
                             )}
                         </Formik>
                     </Col>
                 </Row>
-
             );
         }
     }
 }
-export default connect(() => ({
-}), {
+export default connect(
+    () => ({}),
+    {
         submitToken: tokenSubmitAction,
         checkTokenAndRedirect: checkTokenAndRedirectImportAction,
-    })(CanvasTokenForm);
+    },
+)(CanvasTokenForm);
