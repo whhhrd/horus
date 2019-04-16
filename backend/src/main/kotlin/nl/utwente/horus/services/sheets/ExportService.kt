6ae03b56afd134f737535d0dee5aa2232b1595a2
Rick de Vries <11596702+VriesDeRick@users.noreply.github.com>
2019-04-16 10:51:09 +0200

    Spreadsheet comments (#360)
    * Initial version (only sign off results)
    
    * Add comments for other fields

--- backend/src/main/kotlin/nl/utwente/horus/services/sheets/ExportService.kt
+++ backend/src/main/kotlin/nl/utwente/horus/services/sheets/ExportService.kt
@@ -3,6 +3,7 @@ package nl.utwente.horus.services.sheets
 import nl.utwente.horus.entities.assignment.AssignmentSet
 import nl.utwente.horus.entities.assignment.SignOffResult
 import nl.utwente.horus.entities.assignment.SignOffResultType
+import nl.utwente.horus.entities.comment.CommentThread
 import nl.utwente.horus.entities.course.Course
 import nl.utwente.horus.entities.group.Group
 import nl.utwente.horus.entities.participant.Participant
@@ -42,6 +43,11 @@ class ExportService {
 
         fun nextValue(): Int = value++
     }
+
+    data class CellContent(
+            val body: String,
+            val comments: List<String>?
+    )
 
     companion object {
         const val COMPLETE_STR = "C"
@@ -89,12 +95,12 @@ class ExportService {
 
     private fun addPeopleSheet(participants: Stream<Participant>, sheet: XSSFSheet) {
         val counter = nextCounter(Counter())
-        val header = listOf("Login ID", "Name (sortable)", "Name (full)", "Email", "Labels")
+        val header = listOf("Login ID", "Name (sortable)", "Name (full)", "Email", "Labels").map { CellContent(it, null) }
         appendInRow(sheet.createRow(counter()), header)
         participants.filter { it.role.id == 1L }.forEach { participant ->
             val person = participant.person
             val labels = participant.labels.joinToString { label -> label.name }
-            val contents = listOf(person.loginId, person.sortableName, person.fullName, person.email ?: "", labels)
+            val contents = listOf(person.loginId, person.sortableName, person.fullName, person.email ?: "", labels).map { CellContent(it, null) }
             appendInRow(sheet.createRow(counter()), contents)
         }
         sheet.createFreezePane(0, 1)
@@ -158,21 +164,26 @@ class ExportService {
     }
 
     private fun fillHeaderRow(row: XSSFRow, set: AssignmentSet) {
-        val contents = listOf("S-number", "Name", "Group-ID", "Group name") +
-                set.assignments.map { it.name }
-        appendInRow(row, contents)
+        val preContents = listOf("S-number", "Name", "Group-ID", "Group name").map { CellContent(it, null) }
+        val postContents = set.assignments.map { CellContent(it.name, threadToStrings(it.commentThread)) }
+
+        appendInRow(row, preContents + postContents)
     }
 
     private fun fillParticipantRow(row: XSSFRow, set: AssignmentSet, participant: Participant, group: Group, results: List<SignOffResult>) {
         val person = participant.person
-        val preCells = listOf(person.loginId, person.sortableName, group.id.toString(), group.name)
+        val preCells = listOf(
+                CellContent(person.loginId, null),
+                CellContent(person.sortableName, threadToStrings(participant.commentThread)),
+                CellContent(group.id.toString(), null),
+                CellContent(group.name, threadToStrings(group.commentThread)))
         appendInRow(row, preCells)
 
         if (results.isEmpty()) {
             return // No more of row to fill
         }
 
-        val postCells = LinkedList<SignOffResultType?>()
+        val postCells = LinkedList<SignOffResult?>()
         val assignmentIterator = set.assignments.iterator()
         val resultsIterator = results.iterator()
         var result = resultsIterator.next()
@@ -180,7 +191,7 @@ class ExportService {
             val assignment = assignmentIterator.next()
             if (assignment.id == result.assignment.id) {
                 // This is correct assignment
-                postCells.add(result.result)
+                postCells.add(result)
                 if (resultsIterator.hasNext()) {
                     result = resultsIterator.next() // Advance assignment
                 } else {
@@ -191,17 +202,22 @@ class ExportService {
                 postCells.add(null)
             }
         }
-        appendInRow(row, postCells.map(this::resultToString))
+        appendInRow(row, postCells.map(this::resultToCellContent))
     }
 
-    private fun resultToString(r: SignOffResultType?): String {
+    private fun threadToStrings(thread: CommentThread?): List<String>? {
+        return thread?.let { t -> t.comments.map { "${it.author.person.fullName}: ${it.content}" } }
+    }
+
+    private fun resultToCellContent(r: SignOffResult?): CellContent {
         if (r == null) {
-            return ""
+            return CellContent("", null)
         }
-        if (r == SignOffResultType.COMPLETE) {
-            return COMPLETE_STR
+        val comments = threadToStrings(r.commentThread)
+        if (r.result == SignOffResultType.COMPLETE) {
+            return CellContent(COMPLETE_STR, comments)
         } else {
-            return INCOMPLETE_STR
+            return CellContent(INCOMPLETE_STR, comments)
         }
     }
 
@@ -213,12 +229,26 @@ class ExportService {
         return map
     }
 
-    private fun appendInRow(row: XSSFRow, contents: List<String>) {
+    private fun appendInRow(row: XSSFRow, contents: List<CellContent>) {
         val startNum = max(0, row.lastCellNum.toInt()) // Value is -1 if no rows exist yet...
         val next = nextCounter(Counter(startNum))
         contents.forEach {
             val cell = row.createCell(next())
-            cell.setCellValue(it)
+            cell.setCellValue(it.body)
+            if (it.comments != null) {
+                val sheet = row.sheet
+                val drawing = sheet.drawingPatriarch ?: sheet.createDrawingPatriarch()
+                val factory = sheet.workbook.creationHelper
+                val anchor = factory.createClientAnchor()
+                anchor.setCol1(cell.columnIndex + 1) //the box of the comment starts at this given column...
+                anchor.setCol2(cell.columnIndex + 6) //...and ends at that given column
+                anchor.row1 = cell.rowIndex  // Start on same height as cell
+                anchor.row2 = cell.rowIndex + 2 * it.comments.size //...and 2 rows high per comment line
+
+                val comment = drawing.createCellComment(anchor)
+                comment.string = factory.createRichTextString(it.comments.joinToString("\n"))
+                cell.cellComment = comment
+            }
         }
     }
 
